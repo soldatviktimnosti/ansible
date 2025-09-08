98d35826a582e9ea28a4db2a708909ca3ae331c9
soldatviktimnosti <tishkinsergey123@gmail.com>
2025-09-09 00:30:15 +0300

    теперь всегда клонируется ansible repo

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -107,11 +107,8 @@ cd "$WORK_DIR/vagrant" && vagrant status
 echo "=== Cloning Ansible repo ==="
 cd "$WORK_DIR" || exit
 
-if [ ! -d "vagrant/ansible/.git" ] || \
-   [ "$(git -C vagrant/ansible config --get remote.origin.url)" != "$ANSIBLE_REPO" ]; then
-    rm -rf ./vagrant/ansible
-    git clone "$ANSIBLE_REPO" vagrant/ansible || fail "Failed to clone Ansible repo"
-fi
+rm -rf vagrant/ansible
+git clone "$ANSIBLE_REPO" vagrant/ansible
 
 cd "./vagrant/ansible" || exit 
 
